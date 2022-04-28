bfd1efd5996f1963ec617546b33070221eee278d
natalliaamorim <natallia.nascimneto1@gmail.com>
2022-04-28 01:18:03 -0300

    estados-insta4

--- modulo3/estados-labedin/estados-insta4/insta4/src/App.js
+++ modulo3/estados-labedin/estados-insta4/insta4/src/App.js
@@ -8,10 +8,97 @@ const MainContainer = styled.div`
   flex-direction: column;
   align-items: center;
 `
+const Button = styled.button`
+  text-transform: uppercase;
+  background-color: blue;
+  color: black;
+  height: 3vh;
+  width: 100%;
+  border-style: 2px 2px 2px 2px solid;
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+`;
+
+
 
 class App extends React.Component {
+  
+  state = {
+
+    usuario: [
+
+      {
+        nomeUsuario: "paulinha",
+        fotoUsuario: "bla",
+        fotoPost: "bla"
+      } ,
+
+      {
+        nomeUsuario: "natalia",
+        fotoUsuario: "bla",
+        fotoPost: "bla"
+      },
+
+      {
+        nomeUsuario: "valdivio",
+        fotoUsuario: "bla",
+        fotoPost: "bla"
+      }
+
+
+    ] ,
+
+    valorInputUsuario: "" ,
+    valorInputFoto:"" ,
+    valorInputPost:""
+
+  };
+
+  adicionaUsuario =()=>{
+    const novoUsuario = {
+      nomeUsuario: this.state.valorInputUsuario,
+      fotoUsuario: this.state.valorInputFoto,
+      fotoPost: this.state.valorInputPost
+    };
+
+    const novosUsuarios = [...this.state.usuario,novoUsuario];
+
+    this.setState({usuario:novoUsuario});
+
+  };
+
+  onChangeInputUsuario = (event) =>{
+    this.setState({valorInputUsuario: event.target.value});
+  };
+
+  onChangeInputFoto = (event) =>{
+    this.setState({valorInputFoto: event.target.value});
+  };
+
+  onChangeInputPost = (event) =>{
+    this.setState({valorInputPost: event.target.value});
+  };
+
+
+
   render() {
-    return (
+
+    const listaUsuario = this.state.usuario.map((usuario) => {
+
+      return(
+        <p>
+          {usuario.nomeUsuario} , {usuario.fotoUsuario} , {usuario.fotoPost}
+        </p>
+      )
+
+
+
+    });
+
+
+    return ( 
       <MainContainer>
         <Post
           nomeUsuario={'paulinha'}
@@ -28,13 +115,34 @@ class App extends React.Component {
          <Post
           nomeUsuario={'valdivio'}
           fotoUsuario={'https://picsum.photos/50/53'}
-          fotoPost={'https://picsum.photos/200/151'} 	 
-
 
         />
+          <div>
+          <input
+          value={this.state.valorInputUsuario}
+          onChange={this.onChangeInputUsuario}
+          placeholder={"nomeUsuario"}/>
+          
+
+          <input
+          value={this.state.valorInputFoto}
+          onChange={this.onChangeInputFoto}
+          placeholder={"fotoUsuario"}/>
+          
+
+          <input
+          value={this.state.valorInputPost}
+          onChange={this.onChangeInputPost}
+          placeholder={"fotoPost"}/>
+
+
+        
+          <Button onClick={this.adicionaUsuario}>Adicionar</Button>
+
+        </div> 
       </MainContainer>
     );
-  }
+  };
 }
 
 export default App;
