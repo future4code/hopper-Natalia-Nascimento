db03123d2d47eec5de190f46ed625ba343147e2d
natalliaamorim <natallia.nascimneto1@gmail.com>
2022-08-12 20:03:39 -0300

    lista typescript

--- modulo5/lista-typescript/src/index.ts
+++ modulo5/lista-typescript/src/index.ts
@@ -4,7 +4,7 @@ function recebeString (nome:string, nascimento:string){
     const data= nascimento.split("/")
     return console.log(` Olá me chamo ${nome}, nasci no dia ${data[0]}, no mês ${data[1]} e no ano de ${data[2]}`)
 }
-recebeString("Ana","30/11/1995")
+recebeString("Natalia","19/10/1990")
 //
 // exercício 2
 //
